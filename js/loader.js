9e77377b7d5f9069c29692f3a039007828532e33
Duy Nguyen <duybalu2003@yahoo.com>
2018-12-07 20:39:17 -0500

    fix minor bugs, add loader, clean up codes

---
+++ js/loader.js
@@ -0,0 +1,13 @@
+document.onreadystatechange = function () {
+  var state = document.readyState;
+  if (state == 'interactive') {
+       document.getElementById('body').style.visibility="hidden";
+       document.getElementById('spinner').style.visibility="visible";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('spinner').style.visibility="hidden";
+         document.getElementById('body').style.visibility="visible";
+      },500);
+  }
+}
