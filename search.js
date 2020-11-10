edeb41484bab0a933c00d965c85d9d21fa076c04
Brandon Lokey <57154350+gourmetpez@users.noreply.github.com>
2020-11-10 00:51:12 -0500

    ingrediantSearch can take in comma separated set of ingrediants

--- search.js
+++ search.js
@@ -1,5 +1,8 @@
 function ingrediantSearch() {
-	var ingrediantString = "chicken";
+	var searchString = "chicken,carrots,potatoes";
+	var ingrediantArr = searchString.split(',');
+
+	var ingrediantString = ingrediantArr.map(ingrediant => ingrediant + '%2C');
 
 	let requestString = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/find" +
 	"ByIngredients?number=5&ranking=1&ingredients=";
@@ -44,4 +47,4 @@ function recipeSearch() {
 	});
 }
 
-document.getElementById("btn").addEventListener("click", recipeSearch, true);
+document.getElementById("btn").addEventListener("click", ingrediantSearch, true);
