a9f4c5fbccfa4e50168f33406dbd1a5fe075c6de
Brandon Lokey <57154350+gourmetpez@users.noreply.github.com>
2020-11-10 00:25:46 -0500

    recipeSearch and ingrediantSearch now return json from their queries in test page

--- search.js
+++ search.js
@@ -1,5 +1,12 @@
 function ingrediantSearch() {
-	fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer?q=How%20much%20vitamin%20c%20is%20in%202%20apples%3F", {
+	var ingrediantString = "chicken";
+
+	let requestString = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/find" +
+	"ByIngredients?number=5&ranking=1&ingredients=";
+
+	requestString = requestString + ingrediantString;
+
+	fetch(requestString, {
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-key": "1c21a777d3msh628208c1292df8fp188f2djsn6a46b7c57061",
@@ -16,7 +23,25 @@ function ingrediantSearch() {
 }
 
 function recipeSearch() {
+	var recipeString = "burger";
 
+	let requestString = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=";
+
+	requestString = requestString + recipeString;
+
+	fetch(requestString, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "1c21a777d3msh628208c1292df8fp188f2djsn6a46b7c57061",
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+	}
+	})
+	.then(response => {
+		console.log(response.json());
+	})
+	.catch(err => {
+		console.error(err);
+	});
 }
 
-document.getElementById("btn").addEventListener("click", ingrediantSearch, true);
+document.getElementById("btn").addEventListener("click", recipeSearch, true);
