58123062af437b32cca9fdc5a9d181113c2b121e
Brandon Lokey <57154350+gourmetpez@users.noreply.github.com>
2020-11-10 01:57:45 -0500

    Added question search function

--- search.js
+++ search.js
@@ -47,4 +47,36 @@ function recipeSearch() {
 	});
 }
 
+function questionSearch() {
+	var sentenceString = "How much vitamin C is in an apple?";
+	var encString = encodeURIComponent(sentenceString);
+
+	let requestString = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer?q=";
+
+	requestString = requestString + encString;
+
+	fetch(requestString, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "1c21a777d3msh628208c1292df8fp188f2djsn6a46b7c57061",
+			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+		}
+		})
+		.then(response => {
+			console.log(response.json());
+		})
+		.catch(err => {
+			console.error(err);
+		});
+}
+
+
+function search() {
+	console.log(document.getElementById("searchForm").value);
+	console.log(document.getElementById("searchInput").value);
+}
+
 document.getElementById("btn").addEventListener("click", ingrediantSearch, true);
+document.getElementById("btn2").addEventListener("click", recipeSearch, true);
+document.getElementById("btn3").addEventListener("click", questionSearch, true);
+document.getElementById("searchForm").addEventListener("submit", search, true);
